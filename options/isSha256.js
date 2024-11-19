dadc1ea165a3d3f64bd5587055ec04f4aa6e4091
fosper <fosper@icloud.com>
2024-11-19 15:01:52 +0000

    patch

--- options/isSha256.js
+++ options/isSha256.js
@@ -1,16 +1,17 @@
 'use strict'
 
 export default (optionName, optionValue, optionValueType, path, type, value, inArray) => {
-    const end = (customOptions = {}) => {
-        const options = { code: `OPTION`, field: path, name: optionName, expect: optionValue, given: null, message: null, ...customOptions }
-        return options
+    const end = (customOptions = {}, internalError = false) => {
+        return internalError
+            ? { code: `OPTION`, field: path, name: optionName, message: null, ...customOptions }
+            : { code: `OPTION`, field: path, name: optionName, expect: optionValue, given: null, message: null, ...customOptions }
     }
 
     const availableValueTypes = [ `String` ]
-    if (!availableValueTypes.includes(type)) return end({ message: `Internal field error '${path}'. Unsupported option '${optionName}'.` })
+    if (!availableValueTypes.includes(type)) return end({ message: `Internal field error '${path}'. Unsupported option '${optionName}'.` }, true)
 
     const availableOptionTypes = [ `Boolean` ]
-    if (!availableOptionTypes.includes(optionValueType)) return end({ message: `Internal field error '${path}'. Unsupported option type for option '${optionName}'.` })
+    if (!availableOptionTypes.includes(optionValueType)) return end({ message: `Internal field error '${path}'. Unsupported option type for option '${optionName}'.` }, true)
 
     let result = ``
     let is = false
--- options/values.js
+++ options/values.js
@@ -1,13 +1,14 @@
 'use strict'
 
 export default (optionName, optionValue, optionValueType, path, type, value, inArray) => {
-    const end = (customOptions = {}) => {
-        const options = { code: `OPTION`, field: path, name: optionName, expect: `values`, given: null, message: null, ...customOptions }
-        return options
+    const end = (customOptions = {}, internalError = false) => {
+        return internalError
+            ? { code: `OPTION`, field: path, name: optionName, message: null, ...customOptions }
+            : { code: `OPTION`, field: path, name: optionName, expect: `values`, given: null, message: null, ...customOptions }
     }
 
     const availableOptionTypes = [ `Array` ]
-    if (!availableOptionTypes.includes(optionValueType)) return end({ message: `Internal field error '${path}'. Unsupported option type for option '${optionName}'.` })
+    if (!availableOptionTypes.includes(optionValueType)) return end({ message: `Internal field error '${path}'. Unsupported option type for option '${optionName}'.` }, true)
 
 
     let result = ``
